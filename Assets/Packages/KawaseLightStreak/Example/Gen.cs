8960f79a153164128cba4344ee95248a05aea4f9
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-09-11 18:02:51 +0900

    no message

--- Assets/Packages/KawaseLightStreak/Example/Gen.cs
+++ Assets/Packages/KawaseLightStreak/Example/Gen.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Gen : MonoBehaviour {
+	public enum AlignementEnum { In = 0, On }
+
+	public AlignementEnum alignment;
 	public float size = 1f;
 	public float radius = 10f;
 	public int count = 100;
@@ -10,10 +13,19 @@ public class Gen : MonoBehaviour {
 	void Start () {
 		for (var i = 0; i < count; i++) {
 			var go = (GameObject)Instantiate(prefabs[i % prefabs.Length], 
-			                                 radius * Random.insideUnitSphere, Random.rotationUniform);
+			                                 Position(), Random.rotationUniform);
 			go.layer = gameObject.layer;
 			go.transform.SetParent(transform, false);			
 			go.transform.localScale = size * Vector3.one;
+		}
+	}
+
+	public Vector3 Position() {
+		switch (alignment) {
+		case AlignementEnum.On:
+			return radius * Random.onUnitSphere;
+		default:
+			return radius * Random.insideUnitSphere;
 		}
 	}
 }
