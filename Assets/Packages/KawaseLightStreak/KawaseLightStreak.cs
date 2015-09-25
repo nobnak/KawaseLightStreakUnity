6e229177901558455ac5266e672cf2aa31e85294
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-09-25 17:19:49 +0900

    Save Parameters in XML

--- Assets/Packages/KawaseLightStreak/KawaseLightStreak.cs
+++ Assets/Packages/KawaseLightStreak/KawaseLightStreak.cs
@@ -31,11 +31,11 @@ namespace KawaseLightStreak {
 
 		void OnRenderImage(RenderTexture src, RenderTexture dst) {
 			var lowSrc = src;
-			switch (data.filter) {
+			switch (data.data.filter) {
 			case LightStreakData.FilterModeEnum.Pyramid:
-				if (data.lod > 0) {
+				if (data.data.lod > 0) {
 					lowSrc = Half(src, true);
-					for (var i = 1; i < data.lod; i++) {
+					for (var i = 1; i < data.data.lod; i++) {
 						var lowDst = Half(lowSrc, true);
 						RenderTexture.ReleaseTemporary(lowSrc);
 						lowSrc = lowDst;
@@ -43,7 +43,7 @@ namespace KawaseLightStreak {
 				}
 				break;
 			case LightStreakData.FilterModeEnum.Direct:
-				lowSrc = RenderTexture.GetTemporary(src.width >> data.lod, src.height >> data.lod, 0, src.format);
+				lowSrc = RenderTexture.GetTemporary(src.width >> data.data.lod, src.height >> data.data.lod, 0, src.format);
 				data.kawase.EnableKeyword(KEYWORD_GAMMA_OFF);
 				Graphics.Blit(src, lowSrc, data.kawase, PASS_FLIP_COPY);
 				break;
@@ -66,23 +66,28 @@ namespace KawaseLightStreak {
 				break;
 			}
 			
-			if (data.lod > 0)
+			if (data.data.lod > 0)
 				RenderTexture.ReleaseTemporary(lowSrc);
 		}
 		void OnDestroy() {
 			ReleaseGlowTex();
+			if (!Application.isPlaying)
+				data.Save();
 		}
 		void OnGUI() {
 			if (CheckCamera() && guiOn)
 				_win = GUILayout.Window(0, _win, Window, "UI");
 		}
 		void Start() {
-			data.Load ();
+			data.Load();
+			data.Apply();
 		}
 		void Update() {
 			if (CheckCamera() && Input.GetKeyDown(guiKey)) {
 				guiOn = !guiOn;
 				Cursor.visible = guiOn;
+				if (!guiOn)
+					data.Save();
 			}
 		}
 
@@ -104,14 +109,14 @@ namespace KawaseLightStreak {
 			Clear(_glowTex);
 		}
 		void StarGlow(RenderTexture lowSrc, RenderTexture dst) {
-			var shapeNum = LightStreakData.ShapeNums [(int)data.shape];
+			var shapeNum = LightStreakData.ShapeNums [(int)data.data.shape];
 			var rt0 = RenderTexture.GetTemporary (lowSrc.width, lowSrc.height, 0, lowSrc.format, RenderTextureReadWrite.Linear);
 			var rt1 = RenderTexture.GetTemporary (lowSrc.width, lowSrc.height, 0, lowSrc.format, RenderTextureReadWrite.Linear);
 			var dtheta = TWO_PI / shapeNum;
 			for (var i = 0; i < shapeNum; i++) {
 				rt0.DiscardContents ();
 				Graphics.Blit (lowSrc, rt0, data.kawase, PASS_BRIGHT);
-				LightStreak (ref rt0, ref rt1, i * dtheta + data.angle * Mathf.Deg2Rad);
+				LightStreak (ref rt0, ref rt1, i * dtheta + data.data.angle * Mathf.Deg2Rad);
 				Graphics.Blit (rt0, dst, data.kawase, PASS_ADD);
 			}
 			RenderTexture.ReleaseTemporary (rt0);
@@ -142,40 +147,40 @@ namespace KawaseLightStreak {
 		void Window(int id) {
 			GUILayout.BeginVertical(GUILayout.Width(200));
 
-			var gain = data.kawase_gain;
+			var gain = data.data.kawase_gain;
 			GUILayout.Label(string.Format("Gain:{0:f2}", gain));
 			gain = GUILayout.HorizontalSlider(gain, 0f, 1f);
-			data.kawase_gain = gain;
+			data.data.kawase_gain = gain;
 
-			var thresh = data.kawase_threshold;
+			var thresh = data.data.kawase_threshold;
 			GUILayout.Label (string.Format ("Threshold:{0:f2}", thresh));
 			thresh = GUILayout.HorizontalSlider (thresh, 0f, 1f);
-			data.kawase_threshold = thresh;
+			data.data.kawase_threshold = thresh;
 
-			GUILayout.Label(string.Format("Atten:{0:f3}", data.atten));
-			data.atten = GUILayout.HorizontalSlider(data.atten, 0.9f, 0.95f);
+			GUILayout.Label(string.Format("Atten:{0:f3}", data.data.atten));
+			data.data.atten = GUILayout.HorizontalSlider(data.data.atten, 0.9f, 0.95f);
 
-			GUILayout.Label(string.Format("Angle:{0:f1}", data.angle));
-			data.angle = GUILayout.HorizontalSlider(data.angle, 0f, 360f);
+			GUILayout.Label(string.Format("Angle:{0:f1}", data.data.angle));
+			data.data.angle = GUILayout.HorizontalSlider(data.data.angle, 0f, 360f);
 
 			GUILayout.Label("Shape");
-			data.shape = (LightStreakData.ShapeEnum)GUILayout.Toolbar((int)data.shape, LightStreakData.ShapeLabels);
+			data.data.shape = (LightStreakData.ShapeEnum)GUILayout.Toolbar((int)data.data.shape, LightStreakData.ShapeLabels);
 
 			GUILayout.Label("Filter");
-			data.filter = (LightStreakData.FilterModeEnum)GUILayout.Toolbar((int)data.filter, LightStreakData.FilterModeLabels);
+			data.data.filter = (LightStreakData.FilterModeEnum)GUILayout.Toolbar((int)data.data.filter, LightStreakData.FilterModeLabels);
 
 			GUILayout.EndVertical();
 			GUI.DragWindow();
 
-			data.Load ();
+			data.Apply ();
 		}
 
 		void LightStreak(ref RenderTexture rt0, ref RenderTexture rt1, float dirInRad) {
-			for (var i = 0; i < data.n; i++) {
+			for (var i = 0; i < data.data.n; i++) {
 				var texelOffset = 1;
 				for (var j = 0; j < i; j++)
 					texelOffset *= 4;
-				var absorb = Mathf.Pow (data.atten, texelOffset);
+				var absorb = Mathf.Pow (data.data.atten, texelOffset);
 				data.kawase.SetFloat(LightStreakData.PROP_OFFSET, texelOffset);
 				data.kawase.SetFloat(LightStreakData.PROP_ATTEN, absorb);
 				data.kawase.SetVector(LightStreakData.PROP_DIR, new Vector4(Mathf.Cos(dirInRad), Mathf.Sin(dirInRad), 0, 0));
--- Assets/Packages/KawaseLightStreak/LightStreakData.cs
+++ Assets/Packages/KawaseLightStreak/LightStreakData.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace KawaseLightStreak {
 
@@ -17,22 +19,45 @@ namespace KawaseLightStreak {
 		public const string PROP_ATTEN = "_Atten";
 		public const string PROP_THRESHOLD = "_Thresh";
 		public const string PROP_BOUNDARY = "_Boundary";
-		
-		public FilterModeEnum filter;
-		public ShapeEnum shape = ShapeEnum.Cross;
-		public float atten = 0.95f;
-		public int n = 3;
-		public int lod = 2;
-		public float angle = 45f;
 
+		public string savePath = "LightStreakData.xml";
 		public Material kawase;
-		public float kawase_gain;
-		public float kawase_threshold;
+		public Data data = new Data();
 
-		public void Load() {
-			kawase.SetFloat(PROP_GAIN, kawase_gain);
-			kawase.SetFloat(PROP_THRESHOLD, kawase_threshold);
-			kawase.SetInt(PROP_BOUNDARY, n * 4);
+		[System.Serializable]
+		public class Data {
+			public FilterModeEnum filter;
+			public ShapeEnum shape = ShapeEnum.Cross;
+			public float atten = 0.95f;
+			public int n = 3;
+			public int lod = 2;
+			public float angle = 45f;
+			public float kawase_gain;
+			public float kawase_threshold;
 		}
+
+		public void Apply() {
+			kawase.SetFloat(PROP_GAIN, data.kawase_gain);
+			kawase.SetFloat(PROP_THRESHOLD, data.kawase_threshold);
+			kawase.SetInt(PROP_BOUNDARY, data.n * 4);
+		}
+		public void Save() {
+			using (var writer = new StreamWriter(DataFullPath, false, System.Text.Encoding.UTF8))
+				Serializer.Serialize(writer, data);
+		}
+		public void Load() {
+			if (!string.IsNullOrEmpty(savePath)) {
+				var fullPath = DataFullPath;
+				if (File.Exists(fullPath)) {
+					using (var reader = new StreamReader(fullPath, System.Text.Encoding.UTF8))
+						data = (Data)Serializer.Deserialize(reader);
+				} else {
+					Debug.LogFormat("Serialized Data Not found of type {0} at {1}", this.GetType(), fullPath);
+				}
+			}
+		}
+
+		public string DataFullPath { get { return Path.Combine(Application.streamingAssetsPath, savePath); } }
+		public XmlSerializer Serializer { get { return new XmlSerializer(typeof(Data)); } }
 	}
 }
